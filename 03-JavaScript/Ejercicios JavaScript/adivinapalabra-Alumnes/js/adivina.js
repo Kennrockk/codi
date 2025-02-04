92edb7037a6ae1288539bff1699419183513bf28
Kennrockk <kalvarezzambrano@gmail.com>
2025-02-04 20:15:07 +0100

    nuevo

--- 03-JavaScript/Ejercicios JavaScript/adivinapalabra-Alumnes/js/adivina.js
+++ 03-JavaScript/Ejercicios JavaScript/adivinapalabra-Alumnes/js/adivina.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Variables del juego
-  let pSecreta = "";
-  console.log(pSecreta);
   let pista = "";
-  console.log(pista);
   let lIncorrectas = [];
   console.log(lIncorrectas);
   let intentos = 0;
-  console.log(intentos);
-  let acierto = "";
-  let fallo = "";
+  let acierto = Math.floor(Math.random() * msg.length);
+  let macierto = msg[acierto];
+  let fallo = Math.floor(Math.random() * msgError.length);
+  let mfallo = msgError[fallo];
 
-  // Elementos del DOM
   const inputLetra = document.querySelector(".lletra");
   const cPalabra = document.querySelector(".inputs");
   const cPista = document.querySelector(".pista span");
@@ -20,21 +16,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const cMensaje = document.querySelector(".mostra");
   const cReinicio = document.querySelector(".reset-btn");
 
-  // Iniciar juego
   function empezarJuego() {
-    const indice = Math.floor(Math.random() * listado.length);
-    pSecreta = listado[indice].palabra;
-    console.log(pSecreta);
     pista = listado[indice].pista;
-    console.log(pista);
     for (let i = 0; i < pSecreta.length; i++) {
       const Div = document.createElement("div");
       Div.className = "letras";
       cPalabra.appendChild(Div);
     }
-
-    // Reiniciamos las letras incorrectas y los intentos
-    lIncorrectas = [];
     if (pSecreta.length > 7) {
       intentos = 8;
     } else {
@@ -48,32 +36,34 @@ document.addEventListener("DOMContentLoaded", function () {
     inputLetra.value = "";
     inputLetra.disabled = false;
   }
+  empezarJuego();
+  inputLetra.addEventListener("input", function (e) {
+    const letra = e.target.value.toLowerCase();
+    console.log(letra);
+    if (letra.match(/^[a-zñ]$/)) {
+      comprobarLetra(letra);
+      e.target.value = "";
+    }
+  });
+  let acertado = false;
 
   function comprobarLetra(letra) {
-    let acierto = false;
-
     for (let i = 0; i < pSecreta.length; i++) {
       if (pSecreta[i] === letra) {
         cPalabra.children[i].textContent = letra;
-        acierto = true;
-      } else {
+        acertado = true;
       }
     }
-    if (!acierto) {
+    if (!acertado && !lIncorrectas.includes(letra)) {
+      console.log(lIncorrectas.includes(letra));
       lIncorrectas.push(letra);
       intentos--;
       cErrores.textContent = lIncorrectas;
       cIntentos.textContent = intentos;
     }
-  }
-
-  inputLetra.addEventListener("input", function (e) {
-    const letra = e.target.value.toLowerCase();
-    if (letra.match(/^[a-zñ]$/)) {
-      comprobarLetra(letra);
-      e.target.value = "";
-      console.log((e.target.value = ""));
+    if (intentos === 0) {
+      cMensaje.innerHTML = mfallo;
+      return;
     }
-  });
-  empezarJuego();
+  }
 });
