77e7dad211619fada9eb9b2d9b3fd4d11034751e
Kennrockk <kalvarezzambrano@gmail.com>
2025-02-07 20:25:50 +0100

    nuevo

--- 03-JavaScript/Ejercicios JavaScript/adivinapalabra-Alumnes/js/adivina.js
+++ 03-JavaScript/Ejercicios JavaScript/adivinapalabra-Alumnes/js/adivina.js
@@ -12,14 +12,14 @@ const cPista = document.querySelector(".pista span");
 const cIntentos = document.querySelector(".restantes span");
 const cErrores = document.querySelector(".letrasErroneas span");
 const cMensaje = document.querySelector(".mostra");
-const cReinicio = document.querySelector(".reset-btn");
+const cReinicio = document.querySelector(".colorbtn");
 
 function empezarJuego() {
   pista = listado[indice].pista;
   for (let i = 0; i < pSecreta.length; i++) {
     const Div = document.createElement("div");
     Div.id = "oscuro";
-    Div.className = "letras";
+    Div.className = "letras rounded shadow";
     cPalabra.appendChild(Div);
   }
   if (pSecreta.length > 7) {
@@ -58,7 +58,7 @@ function comprobarLetra(letra) {
     cIntentos.textContent = intentos;
   }
   if (intentos === 0) {
-    cMensaje.innerHTML = mfallo;
+    cMensaje.innerHTML = `<p class="fallo">${mfallo}</p></br><img src ="img/mono_triste.gif" alt="monito triste :(" title="Monito triste :(" ><img>`;
     inputLetra.disabled = true;
   } else {
     victoria();
@@ -72,7 +72,7 @@ function victoria() {
     }
   }
   if (palabraCompleta) {
-    cMensaje.innerHTML = macierto;
+    cMensaje.innerHTML = `<p class="acierto">${macierto}</p></br><img src ="img/monos_felices.gif" alt="monitos felices :D" title="Monitos felices :D" ><img>`;
     inputLetra.disabled = true;
   }
 }
@@ -94,3 +94,37 @@ function contraste() {
   bodyContraste.classList.toggle("oscuro");
   mainContraste.classList.toggle("mainOscuro");
 }
+
+const mas = document.querySelector("#font-up");
+const menos = document.querySelector("#font-down");
+
+const getFontSize = () =>
+  parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue("--font-size")
+  );
+
+mas.addEventListener("click", function () {
+  let fontSize = getFontSize();
+  menos.disabled = false;
+  document.documentElement.style.setProperty(
+    "--font-size",
+    `${fontSize * 1.1}`
+  );
+  if (fontSize > 1.75) {
+    mas.disabled = true;
+  }
+  console.log(fontSize);
+});
+
+menos.addEventListener("click", function () {
+  let fontSize = getFontSize();
+  mas.disabled = false;
+  document.documentElement.style.setProperty(
+    "--font-size",
+    `${fontSize * 0.9}`
+  );
+  if (fontSize <= 1) {
+    menos.disabled = true;
+  }
+  console.log(fontSize);
+});
